1512e605ddf51763012f784125ec332c7abc8f06
user <user@channel700.lan>
2021-09-01 11:41:49 +1000

    update

--- tvSchedule.py
+++ tvSchedule.py
@@ -14,12 +14,27 @@ time = now.strftime('%H%M')
 #Monday is 1
 #can have stop then start on same minute, will both be run
 playlist = [
+	{'day': 1, 'start': '1000', 'stop': '', 'record': '', 'file': '/home/user/Videos/Andre Rieu - Life is Beautiful.mp4'},
+	{'day': 1, 'start': '1400', 'stop': '', 'record': '', 'file': '/home/user/Videos/Slim Dusty - Concert For Slim.mp4'},
+
+	{'day': 2, 'start': '1000', 'stop': '', 'record': '', 'file': '/home/user/Videos/Andre Rieu - Magic Of The Musicals.mp4'},
+	{'day': 2, 'start': '1400', 'stop': '', 'record': '', 'file': '/home/user/Videos/Daniel ODonnell - Can You Feel the Love.mp4'},
+
+	{'day': 3, 'start': '1000', 'stop': '', 'record': '', 'file': '/home/user/Videos/Daniel ODonnell - From The Heartland.mp4'},
 	{'day': 3, 'start': '1400', 'stop': '', 'record': '', 'file': '/home/user/Videos/alan-jackson-precious-memories-gospel-songs.mp4'},
+
+	{'day': 4, 'start': '1400', 'stop': '', 'record': '', 'file': '/home/user/Videos/Daniel ODonnell - Rock And Roll Show.mp4'},
+	
 	{'day': 5, 'start': '1100', 'stop': '', 'record': '', 'file': '/home/user/Videos/30 Greatest Hymns with Lyrics.mp4'},
 	{'day': 5, 'start': '1300', 'stop': '1400', 'record': '', 'file': 'rtsp://192.168.60.205:554/1'},#happy hour
 	{'day': 5, 'start': '1400', 'stop': '1500', 'record': True, 'file': 'rtsp://192.168.60.205:554/1'},#happy hour - record
 	{'day': 5, 'start': '1500', 'stop': '1600', 'record': '', 'file': 'rtsp://192.168.60.205:554/1'},#happy hour
+
+	{'day': 6, 'start': '1000', 'stop': '', 'record': '', 'file': '/home/user/Videos/Slim Dusty - The Very Best Of Slim Dusty.mp4'},
+	{'day': 6, 'start': '1400', 'stop': '', 'record': '', 'file': '/home/user/Videos/Andre Rieu - Live In Sydney.mp4'},
+
 	{'day': 7, 'start': '0900', 'stop': '', 'record': '', 'file': '/home/user/Videos/church_karaoke.mp4'},
+	{'day': 7, 'start': '1400', 'stop': '', 'record': '', 'file': '/home/user/Videos/Daniel ODonnell - The Gospel Show.mp4'},
 
 	#{'day': 7, 'start': '1431', 'stop': '1432', 'record': '', 'file': 'rtsp://192.168.60.205:554/1'},#test
 ]
