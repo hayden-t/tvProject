82bc7090da9aabacbed766b38c24704eab15f050
Hayden Thring <hayden@httech.com.au>
2023-05-27 08:23:25 +1000

    update

--- vlc-launch.sh
+++ vlc-launch.sh
@@ -5,10 +5,11 @@ export XDG_RUNTIME_DIR=/run/user/1000
 #export HOME=/home/user/tvProject/ 
 
 path='/home/user/tvProject/'
-playlist='/var/www/html/playlist/playlist.xspf'
-playlist='/home/user/tvProject/temp.xspf'
+playlist='/home/user/tvProject-manager/html/playlist/playlist.xspf'
+#playlist='/home/user/tvProject/temp.xspf'
 
 md5sum $playlist > $path'playlist.md5'
 
 #become vlc, if window closed will be in hang state, use kill -9 pid
-exec /snap/bin/vlc --intf=dummy --extraintf=oldrc --rc-unix=$path'vlc.sock' --rc-fake-tty --loop --random --fullscreen --codec=ffmpeg--log-verbose=0 $playlist
+#--intf=dummy --random
+exec /snap/bin/vlc --extraintf=oldrc --rc-unix=$path'vlc.sock' --rc-fake-tty --loop  --fullscreen --codec=ffmpeg--log-verbose=0 $playlist
